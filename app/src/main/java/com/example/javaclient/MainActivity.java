383195409160ae0b856f124053a9326cb4eb5944
Yoni Yegorov <yoniandvladi@gmail.com>
2020-09-30 16:48:47 +0300

    gui

--- app/src/main/java/com/example/javaclient/MainActivity.java
+++ app/src/main/java/com/example/javaclient/MainActivity.java
@@ -6,8 +6,6 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    int x = 5 + 10;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
